5d94dc11ec9317104728555481aa192dfadcf350
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-11-13 11:18:35 +0530

    Update gprV2.sv

--- 01_Module_Docs/00_General_Purpose_Registers/gprV2.sv
+++ 01_Module_Docs/00_General_Purpose_Registers/gprV2.sv
@@ -1,35 +1,35 @@
 // Synchronous writes and asynchronous reads
 
-module gpr(
-    input logic        clk,
-    input logic        rstn_h,
-    input logic [4:0]  rs1n,
-    input logic [4:0]  rs2n,
-    input logic [4:0]  rdn,
-    input logic [31:0] rdd,
-    input logic        wbe,
+module gpr #(parameter DATA_WIDTH = 32, ADDR_WIDTH = 5)(
+    input logic                    clk,
+    input logic                    rstn_h,
+    input logic [ADDR_WIDTH - 1:0] rs1n,
+    input logic [ADDR_WIDTH - 1:0] rs2n,
+    input logic [ADDR_WIDTH - 1:0] rdn,
+    input logic [DATA_WIDTH - 1:0] rdd,
+    input logic                    wbe,
 
-    output logic [31:0] rs1d,
-    output logic [31:0] rs2d
+    output logic [DATA_WIDTH - 1:0] rs1d,
+    output logic [DATA_WIDTH - 1:0] rs2d
 );
-    logic [31:0] registers [31:0];
+    logic [DATA_WIDTH - 1:0] registers [31:0];
 
     always(@(posedge clk or negedge rstn_h)) begin
         if (!rstn_h) begin
             for(int i = 0; i < 32; i++) begin
-                registers[i] <= 32'b0;
+                registers[i] <= 'b0;
             end
         end
         else begin
-            registers[0] <= 32'b0;
+            registers[0] <= 'b0;
 
-            if (wbe && (rdn != 5'b0)) begin
+            if (wbe && (rdn != 'b0)) begin
                 registers[rdn] <= rdd;
             end 
         end
     end
 
-    assign rs1d = (!rstn_h) ? 32'b0 : registers[rs1n];
-    assign rs2d = (!rstn_h) ? 32'b0 : registers[rs2n];
+    assign rs1d = (!rstn_h) ? 'b0 : registers[rs1n];
+    assign rs2d = (!rstn_h) ? 'b0 : registers[rs2n];
 
 endmodule
